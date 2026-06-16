a2b43e98ee03e0ac25c48afa9d649813a2bbc7e6
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-06-16 11:22:32 +0100

    feat(client/dashboard): add accessor page

---
+++ client/components/dashboard/AccessorDashboard.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import { user } from "@/constant/user";
+import { useState } from "react";
+import LogoutModal from "@/components/LogoutModal";
+import Image from "next/image";
+import { LogOut, Bell, Clock4, Hourglass, CircleCheckBig } from "lucide-react";
+import { useRouter } from "next/navigation";
+import OverviewCards from "@/components/OverviewCards";
+import { OverviewCard } from "@/constant/publisherDashboard";
+import PendingSubmissions from "@/components/PendingSubmission";
+import RecentActivity from "@/components/RecentActivity";
+import ReviewPerformance from "@/components/ReviewPerformance";
+
+import {
+  pendingSubmissions,
+  recentActivity,
+  reviewerPerformance,
+} from "@/constant/reviewerDashboard";
+
+const overviewCards: OverviewCard[] = [
+  {
+    label: "Pending Reviews",
+    value: "24",
+    icon: Clock4,
+    iconColor: "text-[hsla(210,79%,46%,1)]",
+    iconWrapper: "bg-[hsla(208,78%,85%,1)]",
+    bgClass: "bg-[hsl(209,67%,89%)]",
+  },
+  {
+    label: "In Progress",
+    value: "6",
+    icon: Hourglass,
+    iconColor: "text-[hsla(45,100%,51%,1)]",
+    iconWrapper: "bg-[hsla(60,100%,51%,0.2)]",
+    bgClass: "bg-[hsl(45,100%,85%)]",
+  },
+  {
+    label: "Completed",
+    value: "18",
+    icon: CircleCheckBig,
+    iconColor: "text-[hsla(150,90%,24%,1)]",
+    iconWrapper: "bg-[hsla(150,90%,24%,0.2)]",
+    bgClass: "bg-[hsl(150,28%,85%)]",
+  },
+];
+
+export default function AccessorDashboard() {
+  const [modal, setModal] = useState(false);
+  const router = useRouter();
+
+  return (
+    <section className="md:h-full md:overflow-y-auto flex-2 flex flex-col p-4 gap-6 mb-15 md:p-0 md:pb-6">
+      {modal && <LogoutModal onClose={() => setModal(false)} />}
+
+      <div className="flex items-center justify-between md:hidden">
+        <div className="flex-1 flex items-center gap-2">
+          <div>
+            <Image
+              src="/unilaglogo.svg"
+              alt="UNILAG logo"
+              width={50}
+              height={50}
+            />
+          </div>
+
+          <div>
+            <h2 className="font-bold text-md">Unilag Academic Appraisal</h2>
+            <p className="text-sm text-[hsla(0,2%,42%,1)]">
+              Accessor&apos;s Portal
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <button
+            className="border-solid border border-[hsla(0,0%,85%,1)] p-1 rounded-md cursor-pointer"
+            onClick={() => setModal(true)}
+          >
+            <LogOut size={22} />
+          </button>
+
+          <button
+            onClick={() => router.push("/notifications/reviewer")}
+            className="border-solid border border-[hsla(0,0%,85%,1)] p-1 rounded-md cursor-pointer relative"
+          >
+            <div className="bg-[hsla(0,93%,52%,1)] absolute right-1 h-1 w-1 rounded-full"></div>
+
+            <Bell size={22} />
+          </button>
+
+          <div className="cursor-pointer">
+            <Image
+              src={user.avatar}
+              alt="Profile Picture"
+              width={30}
+              height={30}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center md:border-b md:border-b-[hsla(0,0%,85%,1)] md:p-6">
+        <div>
+          <h2 className="text-xl font-bold md:text-2xl lg:text-3xl">
+            Welcome back,{" "}
+            <span>
+              {user.title} {user.firstname} {user.lastname}
+            </span>
+          </h2>
+          <p className="text-[hsla(0,2%,42%,1)] md:text-lg">
+            Review Submissions
+          </p>
+        </div>
+
+        <div className="gap-4 items-center hidden md:flex">
+          <Image
+            src={user.avatar}
+            alt="Profile Picture"
+            width={50}
+            height={50}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 md:px-6 md:border-b md:border-b-[hsla(0,0%,85%,1)] md:pb-10">
+        <h2 className="text-lg font-semibold md:text-xl">Overview</h2>
+        <OverviewCards overviewCards={overviewCards} />
+      </div>
+
+      <div className="flex flex-col gap-2 md:px-6 md:border-b md:border-b-[hsla(0,0%,85%,1)] md:pb-10">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-semibold md:text-xl">
+            Pending Submission
+          </h2>
+          <p className="text-[hsla(210,79%,46%,1)] font-bold cursor-pointer">
+            View all
+          </p>
+        </div>
+        <PendingSubmissions pendingSubmissions={pendingSubmissions} />
+      </div>
+
+      <div className="flex flex-col gap-2 md:px-6 md:border-b md:border-b-[hsla(0,0%,85%,1)] md:pb-10">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-semibold md:text-xl">Recent Activity</h2>
+          <p className="text-[hsla(210,79%,46%,1)] font-bold cursor-pointer">
+            View all
+          </p>
+        </div>
+        <RecentActivity recentActivity={recentActivity} />
+      </div>
+
+      <div className="flex flex-col gap-2 md:px-6">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-semibold md:text-xl">
+            Review Performance
+          </h2>
+          <p className="text-[hsla(210,79%,46%,1)] font-bold cursor-pointer">
+            View all
+          </p>
+        </div>
+        <ReviewPerformance reviewPerformance={reviewerPerformance} />
+      </div>
+    </section>
+  );
+}
--- client/src/app/(pages)/dashboard/page.tsx
+++ client/src/app/(pages)/dashboard/page.tsx
@@ -1,7 +1,8 @@
 import VCDashboard from "@/components/dashboard/VCDashboard";
 import { user } from "@/constant/user";
 import HRMDDashboard from "@/components/dashboard/HRMDDashboard";
+import AccessorDashboard from "@/components/dashboard/AccessorDashboard";
 
 export default function DashboardPage() {
-  return <VCDashboard />;
+  return <AccessorDashboard />;
 }
